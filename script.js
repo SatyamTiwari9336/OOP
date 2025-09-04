e404e6bc8f18362d4c5797973a35d2a04052c3bd
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-09-04 17:23:48 +0530

    new function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+const Person = function (firstname, birthyear) {
+  this.firstname = firstname;
+  this.birthyear = birthyear;
+};
+
+const satyam = new Person('satyam', 2003);
+console.log(satyam);
