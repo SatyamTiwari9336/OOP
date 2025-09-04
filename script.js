c8a2f7ed6f6421a32163c15c88ed5078e46b61a4
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-09-04 17:45:17 +0530

    new function

--- script.js
+++ script.js
@@ -2,10 +2,23 @@
 const Person = function (firstname, birthyear) {
   this.firstname = firstname;
   this.birthyear = birthyear;
+
+  //never do this
+  //   this.Calcage = function () {
+  //     console.log(2037 - this.birthyear);
+  //   };
 };
 //1.a new {} is created
 //2.function is called , this ={}
 //3.{} linked to prototype
 //4. function automatically return {}
+
 const satyam = new Person('satyam', 2003);
 console.log(satyam);
+const jonas = new Person('jonas', 1975);
+const matilda = new Person('matilda', 2004);
+console.log(matilda, jonas);
+
+//satyam is an instance of Person
+console.log(satyam instanceof Person);
+// never create a method inside a constructor function
