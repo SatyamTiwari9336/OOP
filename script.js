d91643a3c8f3ad3788248d6a8586355550e409fb
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-09-04 17:26:38 +0530

    new function

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const Person = function (firstname, birthyear) {
   this.firstname = firstname;
   this.birthyear = birthyear;
 };
-
+//1.a new {} is created
+//2.function is called , this ={}
+//3.{} linked to prototype
+//4. function automatically return {}
 const satyam = new Person('satyam', 2003);
 console.log(satyam);
