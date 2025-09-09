b98ad46073be2299f4c41c14f644ed91b0746d33
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-09-09 14:56:36 +0530

    class inheritence

--- script.js
+++ script.js
@@ -339,3 +339,29 @@ console.log(newev);
 newev.introduce();
 newev.accelerate();
 console.log(newev); // speed is increased by 20 nd not 10 as child overrite parent methods
+////////////////////////////////////
+//inheritence between clsses using object.create
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.birthYear = birthYear;
+    this.firstName = firstName;
+  },
+};
+
+const steven = Object.create(PersonProto);
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study in ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('jay', 2010, 'Civil Engineering');
+jay.introduce();
+jay.calcAge();
